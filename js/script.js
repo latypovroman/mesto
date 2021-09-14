f7e952cc1b21b8cc1315ce847fffa31ec2209acf
Roman Latypov <jiggawhat5@gmail.com>
2021-09-14 18:43:56 +0500

    feat checklist changes

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let page = document.querySelector('.page');
+
+function switchPopup() {
+  popup.classList.toggle('popup_opened');
+  page.classList.toggle('page_scroll_disable');
+}
+
+function changeProfile(evt) {
+  evt.preventDefault();
+  let name = document.querySelector('#name');
+  let description = document.querySelector('#description');
+  let profileName = document.querySelector('.profile__name');
+  let profileDescription = document.querySelector('.profile__description');
+  profileName.textContent = name.value;
+  profileDescription.textContent = description.value;
+  switchPopup();
+}
+
+function closePopupForce(event) {
+  if (event.target === event.currentTarget) {
+    switchPopup()
+  }
+}
+
+formElement.addEventListener('submit', changeProfile);
+editButton.addEventListener('click', switchPopup);
+popupClose.addEventListener('click', switchPopup);
+popup.addEventListener('click', closePopupForce);
