b72cd2a0b680e66691382275be53d3b34c01b912
Roman Latypov <jiggawhat5@gmail.com>
2021-09-27 16:50:04 +0500

    cards render with js

--- js/script.js
+++ js/script.js
@@ -7,6 +7,46 @@ let name = document.querySelector('#name');
 let description = document.querySelector('#description');
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
+const cardList = document.querySelector('.cards__list');
+const cardTemplate = document.querySelector('.card-template');
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+// Рендер начальных карт start
+function initialCardsRender(initialCards) {
+  const newCard = cardTemplate.content.cloneNode(true);
+  newCard.querySelector('.card__image').src = initialCards.link;
+  newCard.querySelector('.card__title').textContent = initialCards.name;
+
+  cardList.prepend(newCard);
+}
+
+initialCards.map(initialCardsRender);
+// Рендер начальных карт end
 
 function switchPopup() {
   popup.classList.toggle('popup_opened');
