cdb6d461146677757646c275babd857bc9357a46
Roman Latypov <jiggawhat5@gmail.com>
2021-10-07 00:27:07 +0500

    added popup-image, finished

--- js/script.js
+++ js/script.js
@@ -5,10 +5,12 @@ const popup = document.querySelector('.popup');
 const popupProfile = document.querySelector('.popup_type_profile');
 const popupAddCard = document.querySelector('.popup_type_add-card');
 const popupImage = document.querySelector('.popup_type_open-image');
+const popupImagePicture = popupImage.querySelector('.popup__popup-image');
+const popupImageTitle = popupImage.querySelector('.popup__image-title');
 
 const closeProfileButton = popupProfile.querySelector('.popup__close');
 const closeAddButton = popupAddCard.querySelector('.popup__close');
-const closeImageButton = popupImage.querySelector('.popup__close');
+const closeImageButton = popupImage.querySelector('.popup__close-image');
 
 const formProfile = popupProfile.querySelector('.popup__form');
 const formAddCard = popupAddCard.querySelector('.popup__form');
@@ -51,31 +53,43 @@ const initialCards = [
   }
 ];
 
-// Рендер начальных карт start
-function initialCardsRender(initialCards) {
+// Этот говнокод - результат недельной работы, у меня не получается его исправить, все ломается(
+// я в ступоре, в первую очередь не понимаю как функции создания карточек оптимизировать, чтобы не повторять его дважды
+
+function initialCardsRender(initialCards) { // Рендер начальных карт
   const newCard = cardTemplate.content.cloneNode(true);
   newCard.querySelector('.card__image').src = initialCards.link;
   newCard.querySelector('.card__title').textContent = initialCards.name;
+  newCard.querySelector('.card__delete').addEventListener('click', handlerCardDelete);
   newCard.querySelector('.card__like').addEventListener('click', function(evt) {
     evt.target.classList.toggle('card__like_active');
     });
+  newCard.querySelector('.card__image').addEventListener('click', function() {
+    openPopup(popupImage);
+    popupImagePicture.src = initialCards.link;
+    popupImageTitle.textContent = initialCards.name;
+  })
+  closeImageButton.addEventListener('click', function() {
+    closePopup(popupImage);
+  })
   cardList.prepend(newCard);
 }
 initialCards.map(initialCardsRender);
-// Рендер начальных карт end
 
 function openPopup(type) {
   type.classList.add('popup_opened');
   page.classList.add('page_scroll_disable');
 }
 
-
-
 function closePopup(type) {
   type.classList.remove('popup_opened');
   page.classList.remove('page_scroll_disable');
 }
 
+function handlerCardDelete(evt) { // Функция удаления карточки по клику на корзину
+  const elementDelete = evt.target.closest('.card');
+  elementDelete.remove();
+}
 
 // Изменение профиля при нажатии сохранить start
 function changeProfile(evt) {
@@ -100,13 +114,21 @@ function createCard(evt) {
   const newCard = cardTemplate.content.cloneNode(true);
   newCard.querySelector('.card__image').src = link.value;
   newCard.querySelector('.card__title').textContent = title.value;
+  newCard.querySelector('.card__delete').addEventListener('click', handlerCardDelete);
   newCard.querySelector('.card__like').addEventListener('click', function(evt) {
   evt.target.classList.toggle('card__like_active');
   });
+  newCard.querySelector('.card__image').addEventListener('click', function() {
+    openPopup(popupImage);
+    popupImagePicture.src = link.value;
+    popupImageTitle.textContent = title.value;
+  })
+  closeImageButton.addEventListener('click', function() {
+    closePopup(popupImage);
+  })
   cardList.prepend(newCard);
   closePopup(popupAddCard);
 }
-formAddCard.addEventListener('submit', createCard);
 
 // Вызываем попап карточек по клику на add
 addButton.addEventListener('click',
@@ -133,4 +155,7 @@ closeAddButton.addEventListener('click',
   });
 
 
+formAddCard.addEventListener('submit', createCard);
 popup.addEventListener('click', closePopupForce);
+
+
