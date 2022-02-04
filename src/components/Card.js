19c0d0861a2d546dfd4259003ffbe37cbcc5526d
Roman Latypov <jiggawhat5@gmail.com>
2022-02-05 03:44:17 +0500

    added card render and profile render

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,12 @@
 export default class Card {
-  constructor(data, cardSelector, handleCardClick) {
+  constructor(data, cardSelector, {user, handleCardClick, handleDeleteClick}) {
     this._cardSelector = cardSelector;
     this._link = data.link;
     this._name = data.name;
     this._handleCardClick = handleCardClick;
+    this._handleDeleteClick = handleDeleteClick;
+    // this._handleLikeClick = handleLikeClick;
+    this._user = user;
 
   }
 
@@ -16,7 +19,7 @@ export default class Card {
     this._cardImage = this._template.querySelector('.card__image');
     this._cardText = this._template.querySelector('.card__title');
     this._cardDelete = this._template.querySelector('.card__delete');
-    this._cardLike = this._template.querySelector('.card__like');
+    this._cardLike = this._template.querySelector('.card__like-btn');
 
     this._cardImage.src = this._link;
     this._cardImage.alt = this._name;
@@ -28,23 +31,31 @@ export default class Card {
   }
 
   _setEventListeners() {
-    this._cardDelete.addEventListener('click', () => {
-      this._handlerCardDelete();
+    this._cardDelete.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      this._handleDeleteClick();
     });
     this._cardLike.addEventListener('click', () => {
-      this._likeToggle();
+      this._handleLikeClick();
     });
     this._cardImage.addEventListener('click', () => {
       this._handleCardClick(this._name, this._link);
     });
   }
 
-  _handlerCardDelete() {
+  deleteCard() {
     this._cardDelete.closest('.card').remove();
   }
 
-  _likeToggle() {
-    this._cardLike.classList.toggle('card__like_active');
+  // didILike({likes}) {
+  //   likes.map((item) {
+  //     item._id === this._user
+  //   })
+  //   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
+  // }
+
+  likeToggle() {
+    this._cardLike.classList.toggle('card__like-btn_active');
   }
 
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import PopupWithImage from "../components/PopupWithImage.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import Section from "../components/Section.js"
 import UserInfo from "../components/UserInfo.js";
+import Api from "../components/Api";
 import {validateObject} from "../utils/constants.js";
 
 import './index.css';
@@ -21,47 +22,8 @@ const name = document.querySelector('#nickname');
 const description = document.querySelector('#description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-
-// GET INITIALCARDS
-fetch('https://mesto.nomoreparties.co/v1/cohort-34/cards', {
-  headers: {
-    authorization: '6ac52070-ddcb-4a60-a12e-e7350f58493c'
-  }
-})
-  .then(res => res.json())
-  .then((result) => {
-    const initialCardList = new Section({
-      data: result,
-      renderer: (item) => {
-        initialCardList.setItem(createCard(item));
-      }
-    }, '.cards__list');
-    initialCardList.renderItems();
-  })
-  .catch((err) => {
-    console.log(err)
-  });
-
-  fetch('https://nomoreparties.co/v1/cohort-34/users/me', {
-    method: 'PATCH',
-    headers: {
-      authorization: '6ac52070-ddcb-4a60-a12e-e7350f58493c',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      name: 'Marie Skłodowska Curie',
-      about: 'Physicist and Chemist'
-    })
-  })
-    .then(res => res.json())
-    .then((reso) => {
-      const nickname = reso.name;
-      const description = reso.about;
-      userInfo.setUserInfo({nickname, description});
-    })
-    .catch((err) => {
-      console.log(err)
-    });
+const cardList = document.querySelector('.cards__list');
+let user;
 
 // настройка валидации
 const profileValidation =  new FormValidator(validateObject, formProfile);
@@ -73,42 +35,139 @@ initialValidationForms.forEach( function(item) {
   item.enableValidation();
 });
 
-function createCard(data) {
-  const card = new Card(data, ".card-template", handleCardClick);
-  const cardElement = card.generateCard();
-  return cardElement
-}
+const api = new Api({
+  url: 'https://mesto.nomoreparties.co/v1/cohort-34',
+  headers: {
+    authorization: '6ac52070-ddcb-4a60-a12e-e7350f58493c',
+    'Content-Type': 'application/json'},
+});
 
+// GET INITIAL CARDS
 function handleCardClick(name, link) {
-  popupWithImage.open(name, link);
+  popupWithImage.open({name, link});
 }
 
-// попапы профиля и добавления карточки
-const popupWithCard = new PopupWithForm({
-  popupSelector: '.popup_type_add-card',
-  submitAction: (data) => {
-    initialCardList.setItem(createCard(data))
-  }
+function createCard(data) {
+  const card = new Card (
+    data,
+    '.card-template',
+    {handleCardClick,
+      handleDeleteClick: () => {
+      api.deleteCard(data)
+      .then(() => {
+        card.deleteCard()
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+    },
+        // handleLikeClick: () => {
+    //     if (data.likes.length)
+    //     api.deleteLike(data)
+    //       .then((data) => {
+    //         card.handleLikeCard(data)
+    //       })
+    //       .catch((err) => {
+    //         console.log(err)
+    //       });
+
+    //     api.putLike(data)
+    //       .then((data) => {
+    //         card.handleLikeCard(data)
+    //       })
+    //       .catch((err) => {
+    //         console.log(err)
+    //       });
+    // }
+      }
+  );
+  return card.generateCard()
+}
+
+api.getInitialCards()
+.then((result) => {
+  console.log(result);
+  const initialCardList = new Section({
+    data: result,
+    renderer: (item) => {
+      initialCardList.setItem(createCard(item));
+    }
+  }, '.cards__list');
+  initialCardList.renderItems();
 })
+.catch((err) => {
+  console.log(err)
+});
 
-const popupWithImage = new PopupWithImage('.popup_type_open-image');
-
-const popupWithProfile = new PopupWithForm({
-  popupSelector: '.popup_type_profile',
-  submitAction: (data) => {
-    userInfo.setUserInfo(data);
-  }
-})
-
+// GET INITIAL PROFILE INFO
 const userInfo = new UserInfo({
   nameSelector: profileName,
-  infoSelector: profileDescription});
+  infoSelector: profileDescription
+});
 
+api.getUserInfo()
+.then((data) => {
+  console.log(data)
+  const nickname = data.name;
+  const description = data.about;
+  userInfo.setUserInfo({nickname, description});
+  user = data._id;
+})
+.catch((err) => {
+  console.log(err)
+});
+
+
+// PATCH PROFILE INFO
 function getProfileInfo() {
   const information = userInfo.getUserInfo();
   name.value = information.name;
   description.value = information.info;
 }
+
+function changeProfileInfo(data) {
+  return api.patchUserInfo(data)
+        .then((result) => {
+          const nickname = result.name;
+          const description = result.about;
+          userInfo.setUserInfo({nickname, description});
+        });
+}
+
+const popupWithProfile = new PopupWithForm({
+  popupSelector: '.popup_type_profile',
+  submitAction: (data) => {
+    changeProfileInfo(data)
+  }
+})
+
+// POST NEW CARD
+const popupWithCard = new PopupWithForm({
+  popupSelector: '.popup_type_add-card',
+  submitAction: (data) => {
+    api.postNewCard(data)
+    .then((result) => {
+      cardList.prepend(createCard(result));
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+  }
+})
+
+
+
+
+
+const popupWithImage = new PopupWithImage('.popup_type_open-image');
+
+
+
+
+
+
+
+
 
 // слушатели
 popupWithProfile.setEventListeners();
